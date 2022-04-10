a871d09a5a37c870820b5e9e6bd9cd6c0c096621
Modakdipankar <dipankar2022jobs@gmail.com>
2022-04-10 21:33:46 +0530

    Committing changes for Difference and Sum.

--- Diff.java
+++ Diff.java
@@ -3,7 +3,7 @@ public class Diff
   public static void main(String args[])
   {
    int a=100;
-   int b=10;
+   int b=11;
    System.out.println(a-b);
   }
 }
--- Sum.java
+++ Sum.java
@@ -3,7 +3,7 @@ public class Sum
   public static void main(String args[])
   {
    int a=9;
-   int b=10;
+   int b=11;
    System.out.println(a+b);
   }
 }
